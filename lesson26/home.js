ca884267c177b3787478fe8d58e15be1ec1a3742
loranismaili <loranismaili1@gmail.com>
2025-01-27 12:18:39 +0100

    lesson26

---
+++ lesson26/home.js
@@ -0,0 +1,66 @@
+console.log("Goodbye");
+
+var x=5;
+
+var a=50+50;
+console.log(a);
+
+var b=20;
+var c=30;
+var d=b + c;
+console.log(d)
+
+var e=40;
+var f=10;
+var g=e-f;
+console.log(g);
+
+var h=100;
+var i=5;
+var j= h/i;
+console.log(j);
+
+var k=10;
+var l=4;
+var m= k % l;
+console.log(m);
+
+var n=99;
+n++;
+console.log(n);
+
+var o=101;
+o--;
+console.log(o);
+
+var p=20;
+var q= p**2;
+console.log(q)
+
+var x=15;
+x +=5;
+console.log(x);
+
+var s=10;
+s *=7;
+console.log(s);
+
+var v=5;
+var w=5;
+console.log(w==v)
+
+var z=6;
+var t=7;
+console.log (z != t)
+
+var num1=100;
+var num2=10;
+console.log(num1 > num2)
+
+var num3=1;
+var num4=2;
+console.log(num4 < num3)
+
+var num5=123;
+var num6="123";
+console.log(num5 === num6)
