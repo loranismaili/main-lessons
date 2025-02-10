a6cdf2a1bd10f62e73be8e1952bc004ea381d879
loranismaili <loranismaili1@gmail.com>
2025-02-10 12:17:45 +0100

    easy challange 29

---
+++ easy challange 29/main.js
@@ -0,0 +1,17 @@
+var friends=['Loran','andi','klosi','orlus','dinamokus']
+console.log(friends[1]);
+
+friends.push('bigus');
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.unshift('rubikon');
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.splice(0,3);
+console.log(friends);
