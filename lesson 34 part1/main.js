409a8c01473bd15cb6d6ae58d24730b12e6f340a
loranismaili <loranismaili1@gmail.com>
2025-03-05 12:05:27 +0100

    lesson 34 part 1 and 2

---
+++ lesson 34 part1/main.js
@@ -0,0 +1,15 @@
+var colors=['red','green','blue','purple']
+
+function changeBgColor(){
+    document.querySelector('body').style.background=
+    colors[Math.floor(Math.random()*colors.length)];
+}
+
+var names=['Loran','Klos','And','Orli']
+function changeNames(){
+    document.querySelector('p').innerHTML =
+    names[Math.floor(Math.random()*names.length)];
+}
+
+setInterval(changeBgColor,1000);
+setInterval(changeNames, 1000);
